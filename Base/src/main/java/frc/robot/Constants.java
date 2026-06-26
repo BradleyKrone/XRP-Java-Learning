5c6c24e181f57bd954d14ac2018d8f8eb84bbd1e
BradleyKrone <BradleyKroneJ@gmail.com>
2026-06-25 21:15:36 -0500

    setup base repo for java xrp robot

---
+++ Base/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,19 @@
+/**
+ * Constants is one single place to keep important numbers for your robot.
+ *
+ * <p>Things like motor port numbers, driving speeds, or button numbers go here. Keeping them in one
+ * place means that when a number needs to change, you only have to change it once, and it is easy to
+ * find.
+ *
+ * Example of what you might add later:
+ *   - public static final int LEFT_MOTOR_PORT = 0;
+ *   - public static final int RIGHT_MOTOR_PORT = 1;
+ *   - public static final double DRIVE_SPEED = 0.5;
+ *
+ * Note: Only put numbers and simple values here - do not put any robot actions in this file.
+ */
+
+package frc.robot;
+
+
+public final class Constants {}
